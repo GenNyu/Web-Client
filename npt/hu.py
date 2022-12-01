2f9db193c03639161d071198c52749143d067fe8
Phuong Thao Nguyen <89054845+leovingo@users.noreply.github.com>
2022-12-01 10:00:11 +0700

    Update hu.py

--- npt/hu.py
+++ npt/hu.py
@@ -1,24 +1,16 @@
 import socket
 import os
 from threading import Thread
-import tkinter
-
-
-def receive():
-    """Handles receiving of messages."""
-    while True:
-        try:
-            msg = client.recv(1024).decode("utf8")
-            #msg_list.insert(tkinter.END, msg)
-        except OSError:  # Possibly client has left the chat.
-            break
+import time
 #--------------------------------------------------------------
 #Lấy Host và lấy Path
 
 #--------------------------------------------------------------
 def create_request(host,path,method='GET'):
     request =  '{} {} HTTP/1.1\r\nHost: {}\r\nConnection: keep alive\r\n\r\n'.format(method, path, host)
+    print(request)
     return request.encode()
+
 def content_length(client):
     end_of_header = b'\r\n\r\n'
     content_length_check = b'Content-Length:'
@@ -27,8 +19,8 @@ def content_length(client):
     #Receiving the header sent from sever
     try:
         while end_of_header not in header:
-            chunk = client.recv(2) #We receive the header chunk by chunk so we don't accidentally receive the body
-            if not chunk:
+            chunk = client.recv(1) #We receive the header chunk by chunk so we don't accidentally receive the body
+            if not chunk:   
                 break
             else:
                 header += chunk
@@ -47,8 +39,8 @@ def _save_file(Name_file, data):
     f = open(Name_file, 'wb+')
     f.write(data)
     f.close()
-#Hàm lưu dữ liệu vào file tương ứng
-    
+
+#--------------------------------------------------------------
 def content_url(body):
     content_url_check = b'href="'
     list_file = bytes()
@@ -57,8 +49,6 @@ def content_url(body):
         if content_url_check in line:
             list_file += (line.split(content_url_check)[1]).split(b'"')[0] + b"|"
     return list_file.decode()
-    
-#--------------------------------------------------------------
 def get_single_content(HOST, PATH, client):
 #Tạo request và gửi request tới sever
     request = create_request(HOST, PATH)
@@ -84,9 +74,7 @@ def get_single_content(HOST, PATH, client):
                 while end_line not in chunk_info:
                     temp = client.recv(1)
                     chunk_info += temp
-                print(chunk_info)
                 chunk_size = int(chunk_info.decode().split("\r")[0],16)
-                print(chunk_size)
                 if chunk_size == 0 :
                     break
                 temp_data = bytes()
@@ -94,25 +82,22 @@ def get_single_content(HOST, PATH, client):
                 while len(temp_data) < chunk_size:
                     data = client.recv(1)
                     temp_data += data
-                print(len(temp_data))
-                print(chunk_size)
-                data = client.recv(2)
+                data = client.recv(1)
                 body += temp_data
     except socket.timeout:
         print("Connection time out while retrieving the body!!!\n Download failed")
         return
     request_file = PATH.split('/')[-1]
-
     if  PATH == '/' or request_file == 'html' or request_file == "" :
         #index_page
         file_name = 'index.html'
     else: 
         file_name = request_file
     print("Save as file: ", file_name)
+    print("----------------------------")
     _save_file(file_name, body)
     if request_file == "":
         return body
-
 
 
 def get_folder_content(client, HOST, PATH):
@@ -128,31 +113,33 @@ def get_folder_content(client, HOST, PATH):
     url_file_cut = url_file.split('|')[0]
     url_file = url_file.split(url_file_cut)[1]
     for line in url_file.split('|'):
-        print(line)
-        PATH_file = PATH + line
-        t = Thread(target=get_single_content(HOST,PATH_file, client))
-        t.daemon = True
-        t.start()
-
+        if line != "":
+            new_url = "http://"+HOST+PATH+line
+            print("Save file at:", new_url)
+            new_path = PATH+line
+            #get_single_content(HOST, new_path, client)
+            thread_num = 10
+            receive_t = Thread(target=get_single_content(HOST, new_path, client))
+            receive_t.start()
 
 URL = 'http://web.stanford.edu/class/cs143/handouts/'
 HOST = URL.split('/')[2]
-HOST = URL.split('/')[2]
 PATH = URL.split(f'http://{HOST}')[1]
-if PATH == '': 
+if PATH == '':
     PATH = '/'
 PORT = 80
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     client.connect((HOST, PORT))
     print(f'* Running on http://{HOST}:{PORT}')
 except socket.error as e:
     print(f'socket error: {e}')
-if URL[-1]=="/" and URL.count("/") > 3 :
-    get_folder_content(client, HOST, PATH)
-else:
-    get_single_content(HOST, PATH, client)
-
-
+print("start work")
+start = time.time()
+receive_thread = Thread(target=get_folder_content(client, HOST, PATH))
+receive_thread.start()
+end = time.time()
+print(f'Download in {end - start} seconds')
 client.close()
 input('Press ENTER to exit')
